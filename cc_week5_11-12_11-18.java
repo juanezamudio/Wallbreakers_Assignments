49e2100be1ab96a0b02a83c62d12fe93651adc76
Juan Zamudio <jzamudio14@gmail.com>
2018-11-15 02:51:29 -0600

    Up to Problem 4 on Trees

--- cc_week5_11-12_11-18.java
+++ cc_week5_11-12_11-18.java
@@ -57,10 +57,90 @@ class Trees {
      * Same Tree
      */
 
+    /**
+     * Optimal Solution?
+     * 
+     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+     */
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        List<Integer> pList = new ArrayList<Integer>();
+        List<Integer> qList = new ArrayList<Integer>();
+        
+        bfs(p, pList);
+        bfs(q, qList);
+        
+        return pList.equals(qList);
+    }
+    
+    public void bfs(TreeNode root, List<Integer> nodes) {
+        if (root == null) {
+            nodes.add(null);
+        }
+        
+        if (root != null) {
+            nodes.add(root.val);
+            bfs(root.left, nodes);
+            bfs(root.right, nodes);
+        }
+    }
 
     /**
      * Sum of Left Leaves
      */
+
+     /**
+      * Better Solution?
+      *
+      * public int sumOfLeftLeaves(TreeNode root) {
+            if(root == null) return 0;
+            if(root.left!= null && root.left.left == null && root.left.right == null )
+                return root.left.val + sumOfLeftLeaves(root.right);
+            
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+      */
+
+      int sum = 0;
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        if (root == null || (root.left == null && root.right == null)) {
+            return sum;
+        }
+        
+        getLeftLeaves(root);
+        
+        return sum;
+    }
+    
+    public int getLeftLeaves(TreeNode root) {
+        
+        if (root == null) {
+            return sum;
+        }
+        
+        if (root.right != null && root.right.left == null && root.right.right == null) {
+            return getLeftLeaves(root.left);
+        }
+        
+        if (root.left == null && root.right == null) {
+            sum += root.val;
+        } else {
+            getLeftLeaves(root.left);
+            getLeftLeaves(root.right);
+        }
+        
+        return sum;
+    }
 
     /**
      * Diameter of Binary Tree
@@ -76,5 +156,5 @@ class Trees {
 }
 
 class Graphs {
-    
+
 }
